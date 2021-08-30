8e45d4de46cec61e31e6d3fe2b52f20f8bec3df5
Okarin <steinsgate95@mail.ru>
2021-08-30 15:11:55 +0600

    Проект закончен. Добавил удаление дубликатов ссылок. Скачал все изображения

--- main.py
+++ main.py
@@ -39,9 +39,9 @@ def get_html(url):
     driver.find_element_by_class_name('_9AhH0').click()
     driver.implicitly_wait(50)
     # Ввод логина
-    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div/label/input').send_keys('nmaksimov976@gmail.com')
+    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/form/div[1]/div[1]/div/label/input').send_keys('steinsgate95@mail.ru')
     # Ввод пароля
-    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/form/div[1]/div[2]/div/label/input').send_keys('Nik.2005')
+    driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/form/div[1]/div[2]/div/label/input').send_keys('gQ25yz7832')
 
     # Нажатие кноки "Войти"
     driver.find_element_by_xpath('/html/body/div[6]/div[2]/div/div[2]/div/div/div[1]/div/form/div[1]/div[3]/button').click()
@@ -49,17 +49,21 @@ def get_html(url):
     # Нажатие кнопки "Не сейчас"
     driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
 
-    for i in range(21):
-
-        for j in range(3):
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            
-            html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML") # Получение содержимого html-тега
+    # Скролл до самого низа страницы с получением ссылок на все посты
+    for i in range(0, 64):
+        driver.execute_script(f"window.scrollTo({768 * i}, {768 * (0 + i)})") 
+        sleep(2)
         
-            soup = get_soup(html)
-            links = get_src(soup)
-        download_images(links)
+        
+        html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML") # Получение содержимого html-тега
 
+        soup = get_soup(html)
+        links = get_src(soup)
+
+        # Запись ссылок в файл
+        for j in links:
+            with open('links.txt', 'a') as file:
+                file.write(f'{j}\n')
 
 def download_images(links):
     '''
@@ -67,18 +71,49 @@ def download_images(links):
     '''
     os.chdir('C:\\Users\\Никита\\Desktop\\instagram-parser\\img') # Переход в папку img
 
+    # Открытие файла с номером изображения
+    infile = open('count.txt', 'r')
+    count = int(infile.read())
+    infile.close()
 
     # Скачивание каждого файла
-    for i in range(len(links)):
-        name = f'{i}.jpeg' # Имя файла
-        url = links[i] # URL для скачивания файла
+    for link in links:
+        name = f'{count}.jpg'
+        url = link
         urllib.request.urlretrieve(url, name) # Скачивание файла
+        count += 1
 
-
+        # Запись нового значения в файл
+        outfile = open('count.txt', 'w')
+        outfile.write(str(count))
+        outfile.close()
+       
 def main():
     url = 'https://www.instagram.com/mashina_satam/?hl=ru'
 
-    print(get_html(url))
+    get_html(url)
+    
+    # Получение всех ссылок
+    infile = open('links.txt', 'r')
+    text = infile.read()
+
+    # Поиск и удаление дубликатов
+    rows = text.splitlines()
+    unique_rows = list(dict.fromkeys(rows))
+    infile.close()
+
+    # Запись уникальных ссылок в файл
+    outfile = open('links.txt', 'w')
+    for i in unique_rows:
+        outfile.write(i + '\n')
+    outfile.close()
+
+    # Чтение ссылок из файла
+    infile = open('links.txt', 'r')
+    links = infile.readlines()
+    infile.close()
+
+    download_images(links)
 
 if __name__ == '__main__':
     main()
